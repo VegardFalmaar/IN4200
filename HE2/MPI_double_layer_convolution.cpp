c95420bf0c6dbf4cba938af798ab99b98fe8c70b
Vegard Falmaar <vegardfa@student.matnat.uio.no>
2021-05-03 14:22:28 +0200

    Some HE2

--- HE2/MPI_double_layer_convolution.cpp
+++ HE2/MPI_double_layer_convolution.cpp
@@ -10,21 +10,16 @@ void MPI_double_layer_convolution (
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
-  // first layer of convolution
   my_M = M/numprocs;
-  my_N = N/numprocs;
-  i_start = my_rank*my_M;
-  i_stop = (my_rank+1)*my_M;
-  j_start = my_rank*my_N;
-  j_stop = (my_rank+1)*my_N;
+  i_start = (my_rank*M)/numprocs;
+  i_stop = ((my_rank + 1)*M)/numprocs;
 
   my_input = new float*[my_M];
-  contig_array = new float[my_M*my_N];
-  for (int i=0; i<my_M; i++) {
-    my_input[i] = &(contig_array[i*my_N]);
-    for (int j=0; j<my_N; j++)
-      my_input[i][j] = 
-  }
+  contig_array = new float[my_M*N];
+  for (int i=0; i<my_M; i++)
+    my_input[i] = &(contig_array[i*N]);
+
+}
 
 void single_layer_convolution (
   int M, int N, float **input,
